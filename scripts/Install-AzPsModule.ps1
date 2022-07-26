94841ae1548ec53158d4d735162f8be67bc5b856
Dhruv Chand <dhruvchand@live.com>
2022-07-25 17:36:45 -0700

    disable TLS1.2

--- scripts/Install-AzPsModule.ps1
+++ scripts/Install-AzPsModule.ps1
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 # Set TLS1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
+#[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
 
 Write-Host "Installing AZ PowerShell module..."
 Install-Module -Name Az -Scope AllUsers -SkipPublisherCheck -Force
